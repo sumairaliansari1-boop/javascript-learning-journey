b001a7a1a5169772d9d149f9820f765b6597a0c9
sumairaliansari1-boop <sumairaliansari1@gmail.com>
2026-08-03 18:06:52 +0500

    Enhance primitive and reference data type examples
    Updated examples and console logs for clarity on primitive and reference data types.

--- 02-datatypes/primitive-vs-Refrence.js
+++ 02-datatypes/primitive-vs-Refrence.js
@@ -1,10 +1,31 @@
+// primitive Data type Example
+// primitive values are copied by value
+
 let i = 10
 let j = i
 
 i = 20 
 
-console.log(i)
-console.log(j)
+console.log("Prmitive Data type")
+console.log("i =", i);
+console.log("j =", j);
 
 
-// primitive Data type Example
+
+
+// Reference Data type Example
+// Both variables point to the same array in memory
+
+let fruits = [10, 20, 30, 40, 50];
+
+let copied = fruits 
+
+console.log("Before pop():");
+console.log("fruits = ",fruits);
+console.log("copied = ",copied);
+
+copied.pop();
+
+console.log("After pop():");
+console.log("fruits =", fruits);
+console.log("copied =", copied);
