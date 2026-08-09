f9eef3339d561f1c4f0cf9773c2329c1939df011
sumairaliansari1-boop <sumairaliansari1@gmail.com>
2026-08-09 20:42:30 +0500

    Create type conversion playground in JavaScript
    Added a playground for practicing type conversions in JavaScript, including string (), number ( ), and boolean ( ) conversions.

---
+++ 02-datatypes/type_coversion_playGround.js
@@ -0,0 +1,99 @@
+// Type Conversion Playground
+// Practicing String (), Number() and Boolean (), Conversions 
+
+
+// String Conversion
+
+const i = 123; 
+
+//before conversion
+console.log(typeof (i))
+//after conversion
+console.log (String(i));
+console.log (typeof String(i));
+
+
+
+// Second Example
+
+const j = true;
+
+// before conversion
+console.log(typeof(j))
+// after coversion
+console.log(String(j))
+console.log(typeof String(j))
+
+
+
+
+
+// Number Conversion 
+
+const k = "1000";
+
+//before conversion
+console.log(typeof (k))
+//after conversion
+console.log(Number(k))
+console.log(typeof Number(k))
+
+
+
+
+// second Examle
+const l = "110.5";
+//before conversion
+console.log(typeof (l))
+
+// after coversion
+console.log(Number(l))
+console.log(typeof Number(l))
+
+
+
+
+
+
+//Boolean Conversion
+
+const m = 1;
+
+//before conversion
+console.log(typeof (m))
+
+//after conversion
+console.log(Boolean(m))
+console.log(typeof Boolean(m))
+
+
+
+
+// second Example
+
+const n = "";
+
+//before coonversion
+console.log(typeof (n))
+
+// After Conversion
+console.log(Boolean(n))
+console.log(typeof Boolean(n))
+
+
+
+
+
+
+const s = "Hello";
+
+//Before conversion
+console.log(typeof (s))
+
+// After conversion
+console.log(Boolean (s))
+console.log(typeof Boolean(s))
+
+
+
+
