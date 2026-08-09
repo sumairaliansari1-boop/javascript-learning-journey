db339c25e93f627aaa2ce315ab4c9886c3a73c52
sumairaliansari1-boop <sumairaliansari1@gmail.com>
2026-08-09 19:49:18 +0500

    Add Data Type Checker in JavaScript
    This program checks the data type of different JavaScript values using the typeof operator.

---
+++ 02-datatypes/Data_type_Checker.js
@@ -0,0 +1,24 @@
+// Data tpe Checker using typeof
+// This program checks the data type of diffrent JavaScript value
+
+const arr = "Sumair Ali";
+const num = 123;
+const demo = true;
+const i = undefined;
+const j = null;
+const k = ["Sumair", "Umair", "Zakir", "Sabir"];
+const person = { 
+    name : "Sumair",
+    age : 23
+};
+
+
+console.log(typeof arr);
+console.log(typeof num);
+console.log(typeof demo)
+console.log(typeof i)
+console.log(typeof j)
+console.log(typeof k)
+console.log(typeof person)
+
+
