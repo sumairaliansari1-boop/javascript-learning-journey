047570a4a73685d43c0c84779bfadc9f8ebe38c1
sumairaliansari1-boop <sumairaliansari1@gmail.com>
2026-09-11 19:31:39 +0500

    Add simple calculator functions for basic operations

---
+++ 07-Functions/Simple-Calculator-Functions/simple-calculator-functions.js
@@ -0,0 +1,45 @@
+// ****** Addition Number **********
+
+function additionNumber(val1,val2){
+   return  val1 + val2 
+}
+
+
+console.log(additionNumber(22,32))
+
+
+
+// ******* Substraction Function ********
+
+function susbsNumber(num1,num2){
+    return num1 - num2
+  
+}
+
+console.log(susbsNumber(30,14))
+
+
+
+
+
+// ***** Multiplication Function *********
+
+function mulNumber(val1, val2){
+    return val1 * val2
+    
+}
+
+console.log(mulNumber(222,3))
+
+
+
+
+
+// ******** Division Function **********
+
+function divNumber(val1,val2){
+    return val1 / val2
+    
+}
+
+console.log(divNumber(222,12))
