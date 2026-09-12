d69a81fd95ac62c2a1c2d1e22560e9a3290262d7
sumairaliansari1-boop <sumairaliansari1@gmail.com>
2026-09-12 23:43:29 +0500

    Implement student name search functionality

---
+++ 08-Arrays/Search-Student-by-Name/search-student-by-name.js
@@ -0,0 +1,15 @@
+const students = ["Ali", "Ahmed", "Sara", "Ayesha", "Usman"];
+
+const searchName = "Sara";
+
+
+if(students.includes(searchName)){
+  
+  console.log(`${searchName} found in student list`)
+}
+
+else {
+  
+  console.log(`${searchName} Not found in student list`)
+  
+}
