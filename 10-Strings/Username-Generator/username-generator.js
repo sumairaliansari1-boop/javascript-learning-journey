719d54b19dddebbd44bc3488b59c086ef69547f3
sumairaliansari1-boop <sumairaliansari1@gmail.com>
2026-09-23 16:07:45 +0500

    Implement username generator from full name

---
+++ 10-Strings/Username-Generator/username-generator.js
@@ -0,0 +1,11 @@
+const fullName = "  Sumair Ali Ansari  ";
+
+
+let first = fullName.trim()
+let second = first.toLowerCase()
+let finalResult = second.replaceAll(" ","_")
+
+
+console.log(finalResult);
+
+
