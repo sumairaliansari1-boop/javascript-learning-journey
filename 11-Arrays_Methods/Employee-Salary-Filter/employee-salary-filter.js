0a773b5a4f9e843fe03f13af22b6ba5a52ad7f23
sumairaliansari1-boop <sumairaliansari1@gmail.com>
2026-09-24 14:35:22 +0500

    Implement salary filter for employees

---
+++ 11-Arrays_Methods/Employee-Salary-Filter/employee-salary-filter.js
@@ -0,0 +1,15 @@
+const salaries = [25000, 45000, 60000, 35000, 80000, 50000];
+
+const minimumSalary = 50000;
+
+
+let result = salaries.filter((extract)=>{
+    return extract >= minimumSalary
+})
+
+console.log(result);
+
+
+    
+
+
